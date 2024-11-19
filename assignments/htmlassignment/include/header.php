105f2b4040f0e0e1ce2c364f446fca5932696631
ianzzz <yihengchu@icloud.com>
2024-11-19 15:04:57 -0500

    php update

---
+++ assignments/htmlassignment/include/header.php
@@ -0,0 +1,10 @@
+<header style="display: flex; align-items: center; justify-content: space-between;">
+    <img src="img/WechatIMG4464.jpg" alt="Site Logo" class="site-logo">
+    <nav>
+        <ul style="list-style-type: none; padding: 0; margin: 0; display: flex;">
+            <li style="margin-right: 20px;"><a href="index.php">Home</a></li>
+            <li style="margin-right: 20px;"><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
--- assignments/htmlassignment/index.php
+++ assignments/htmlassignment/index.php
@@ -55,17 +55,7 @@
 
 <body>
 
-    <header style="display: flex; align-items: center; justify-content: space-between;">
-        <img src="img/WechatIMG4464.jpg" alt="Site Logo" class="site-logo">
-        <nav>
-            <ul style="list-style-type: none; padding: 0; margin: 0; display: flex;">
-                <li style="margin-right: 20px;"><a href="index.html">Home</a></li>
-                <li style="margin-right: 20px;"><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-
+<?php include 'include/header.php'; ?>
 
     <main>
 
